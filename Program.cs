3b6f0a8cd111834c7953c3114f2742bda288ba72
Sean Deng <dcx07@hotmail.com>
2025-09-18 11:04:50 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Windows.Devices.Sensors;
+
+class Program
+{
+    static void Main()
+    {
+        var sensor = LightSensor.GetDefault();
+        if (sensor == null)
+        {
+            Console.WriteLine("Ambient light sensor not found");
+            return;
+        }
+
+        var reading = sensor.GetCurrentReading();
+        Console.WriteLine($"Light intensity: {reading.IlluminanceInLux} lux");
+    }
+}
